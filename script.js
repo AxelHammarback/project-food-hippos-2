11254fbdc9f8fb96bc033574847e9538eec5fae4
AxelHammarback <axel.hammarback@gmail.com>
2020-09-30 18:09:48 +0200

    Added a loading spinner, and some touch-ups on the design

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const apiKey = "bd7b7a44bc3c3e751cac9af7a9a53345";
 const cityID = 84; // Prague
 const cuisineID = 178; // Kebab
 
-// Prague Europe ID: 84
-// Prague Oklahoma ID: 8294
-
 const sortRatingURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city&cuisines=${cuisineID}&sort=cost&order=asc`;
 const sortCostURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city&cuisines=${cuisineID}&sort=rating`;
 const defaultURL = `https://developers.zomato.com/api/v2.1/search?entity_id=${cityID}&entity_type=city&cuisines=${cuisineID}`;
@@ -17,35 +14,40 @@ const cardAdditionalInfo = document.getElementById('cardAdditionalInfo')
 
 
 const fetchRestaurants = (url) => {
-  console.log(`Nu är vi inne i funktionen, ${url}`)
+  document.getElementById("spinnerContainer").innerHTML = `<div class="loading-spinner"></div>`;
+  document.getElementById("amountOfCards").innerHTML = "";
   mainCardArea.innerHTML = "";
   const restBox = document.getElementById("restaurants");
-// När måste man använda headers och inte?
-// Varför använde Van inte headers i dagens övning?
+
   fetch(url, { headers: {"user-key": apiKey} })
     .then((response) => {
       return response.json();
     })
-        .then((restaurantObject) => {
-          console.log(restaurantObject);
-          restaurantObject.restaurants.forEach((rest) => {
-            // Gör om average cost till avg cost for one.
-            mainCardArea.innerHTML += `
-            
-            <div class="card-main" id="cardMain">
-            <a href="${rest.restaurant.menu_url}" class="link-container" target="_blank"><div class="card-image" id="cardImage"><img src="${checkIfImageExists(rest.restaurant.featured_image)}"/></div>
-            <div class="card-info" id="cardInfo">
+    .then((restaurantObject) => {
+      document.getElementById("spinnerContainer").innerHTML = "";
+      console.log(restaurantObject);
+      restaurantObject.restaurants.forEach((rest) => {
+        mainCardArea.innerHTML += `
+        <div class="card-main" id="cardMain">
+          <a href="${rest.restaurant.menu_url}" class="link-container" target="_blank">
+          <div class="card-image" id="cardImage">
+            <img src="${checkIfImageExists(rest.restaurant.featured_image)}"/>
+          </div>
+          <div class="card-info" id="cardInfo">
             <h3>${rest.restaurant.name}</h3>
             <p>${rest.restaurant.location.address}</p>
-            </div></a>
-            <div class="card-additional-info" id="cardAdditionalInfo">
-            <div class="info-small-box"><p>💰👬${rest.restaurant.average_cost_for_two} ${rest.restaurant.currency}</p>          
+          </div></a>
+          <div class="card-additional-info" id="cardAdditionalInfo">
+            <div class="info-small-box">
+              <p>💰👬${rest.restaurant.average_cost_for_two} ${rest.restaurant.currency}</p>          
             </div>
-            <div class="info-small-box"><p>⭐️${rest.restaurant.user_rating.aggregate_rating}</p>       
+            <div class="info-small-box">
+              <p>⭐️${rest.restaurant.user_rating.aggregate_rating}</p>       
             </div>
-          </div>
-            `
+        </div>
+        `
       });
+      document.getElementById("amountOfCards").innerHTML = `Displaying 20 out of 91`;
   })
 }
 
@@ -53,13 +55,11 @@ const fetchRestaurants = (url) => {
 
 const checkIfImageExists = featuredImage => {
   console.log(featuredImage);
-  
   if (!featuredImage) {
     return `./assets/image_kebab.png`;
   } else {
     return featuredImage;
   }
-  
 }
 
 
